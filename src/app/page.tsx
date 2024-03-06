f23bf9974413a66dd5266a30b2278ebbdacc7098
Diego Pacori <dpacoria@unsa.edu.pe>
2024-03-06 01:13:02 -0500

    Se puso Nextjs fonts a la aplicacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { titleFont } from '@/config/fonts';
 import Image from 'next/image';
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center p-24">
             <p className="text-slate-800 font-bold text-5xl">Teslo Shop</p>
+            <h1 className={`${titleFont.className} font-bold`}>Hola a todos bebecitas</h1>
         </main>
     );
 }
